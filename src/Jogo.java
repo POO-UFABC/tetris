bb241a34a394e4e7291f24346de641be71abdde0
Chokitus <victor.bremello@hotmail.com>
2018-07-10 17:48:28 -0300

    Corrigindo o commit (acho)

--- src/Jogo.java
+++ src/Jogo.java
@@ -3,7 +3,6 @@ import base.*;
 public class Jogo {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		EstruturaFixa fixa = new EstruturaFixa();
 		
 		Bloco[][] pecinha = new Bloco[5][5];
@@ -168,23 +167,5 @@ public class Jogo {
 			str = "";
 		}
 		System.out.println(str);		
-=======
-		Estrutura movel = new Estrutura(0,1);
-		Estrutura fixa = new Estrutura();
-
-		Bloco[][] blocos = new Bloco[5][5];
-		for(int i = 0; i<5; i++)
-			for(int j = 0; j<5;j++)
-				blocos[i][j] = new Bloco("#000000");
-		movel.setBlocos(blocos);
-		
-		blocos = null;
-
-		blocos = new Bloco[30][100];
-		blocos[0][0] = new Bloco("#FF");
-		fixa.setBlocos(blocos);
-
-		System.out.println(movel.existeBlocoAbaixo(fixa));
->>>>>>> 59caabe74c62e3003f3dc109493440105dec47fb
 	}
 }
--- src/base/Bloco.java
+++ src/base/Bloco.java
@@ -1,7 +1,6 @@
 package base;
 
 public class Bloco {
-<<<<<<< HEAD
 	private String cor = " ";
 	public Bloco() {
 		this("#FFFFFF");
@@ -23,19 +22,5 @@ public class Bloco {
 
 	public String getCor(){
 		return (this.cor == null ? " " : this.cor);
-=======
-	private String cor;
-	public Bloco() {
-		this("#FFFFFF");
-	}	
-	public Bloco(String cor) {	
-		setCor(cor);
-	}
-	private void setCor(String cor){
-		this.cor = cor;
-	}
-	private String getCor(){
-		return this.cor;
->>>>>>> 59caabe74c62e3003f3dc109493440105dec47fb
 	}
 }
