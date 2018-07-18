cba4ee80db3e79a1237c720cf45b4c5561ec77a3
Jonas Santos Marma <jonas.marma@gmail.com>
2018-07-17 22:50:01 -0300

    Update MenuState.java

--- tetris/gameStates/MenuState.java
+++ tetris/gameStates/MenuState.java
@@ -7,29 +7,74 @@ import io.KeyManager;
 
 import gameStates.GameState;
 
-public class MenuState extends State{
+public class MenuState extends State{ 
+  private boolean justPressedRight = false;
+  private boolean justPressedLeft = false;
+
 	public void update() {
-		if (State.getKeyManager().getZero()) {
-		  System.out.println("(MENU S) ZERO");
-		  GameState gameState = new GameState();
-		  State.setState(gameState);
-		}
-		if (State.getKeyManager().getDown()) {
-		  System.out.println("(MENU S) DOWN");
-		}
-		if (State.getKeyManager().getLeft()) {
-		  System.out.println("(MENU S) LEFT");
-		}
-		if (State.getKeyManager().getRight()) {
-		  System.out.println("(MENU S) RIGHT");
-		}
+	  if (State.getKeyManager().getZero()) {
+	    System.out.println("(MENU) ZERO");
+	    GameState gameState = new GameState();
+	    State.setState(gameState);
+	  }
+	  if (State.getKeyManager().getDown()) {
+	    System.out.println("(MENU) DOWN");
+	  }
+	  
+	  //Apertar a tecla seta esquerda decrementa a dificuldade
+	  if (State.getKeyManager().getLeft() == true) {
+	    if (this.justPressedLeft == false) {
+	      this.incrementGameMode(-1);
+	      this.justPressedLeft = true;
+	    }
+	  }
+	  if (State.getKeyManager().getLeft() == false) {
+	    this.justPressedLeft = false;
+	  }  
+
+    //Apertar a tecla seta direita incremenra a dificuldade
+	  if (State.getKeyManager().getRight() == true) {
+	    if (this.justPressedRight == false) {
+	      this.incrementGameMode(+1);
+	      this.justPressedRight = true;
+	    }
+	  }
+	  if (State.getKeyManager().getRight() == false) {
+	    this.justPressedRight = false;
+	  }
+	}
+	
+	private void incrementGameMode(int increment) {
+    this.setGameMode(this.getGameMode() + increment);
+    if (this.getGameMode() > 2) {
+      this.setGameMode(0);
+    }
+    else if (this.getGameMode() < 0) {
+      this.setGameMode(2);
+    }
 	}
 
-	public void render(Graphics g) {
-		g.setColor(Color.red);
-		g.fillRect(10, 50, 50, 70);
-		g.setColor(Color.green);
-    g.fillRect(0, 0, 10, 10);
+	public void render(Graphics g) {	  
+    g.setColor(Color.white);
+    g.drawString("=TETRIS=", 210, 200);
+    
+    if (this.getGameMode() == 0){
+      g.drawString(">Facil<", 120, 260);
+      g.drawString("Medio", 200, 260);
+      g.drawString("Dificil", 290, 260);
+	  }
+	  
+	  else if (this.getGameMode() == 1){
+      g.drawString("Facil", 120, 260);
+      g.drawString(">Medio<", 200, 260);
+      g.drawString("Dificil", 290, 260);
+	  }
+	  
+	  else if (this.getGameMode() == 2){
+      g.drawString("Facil", 120, 260);
+      g.drawString("Medio", 200, 260);
+      g.drawString(">Dificil<", 290, 260);
+	  }
 	}
 
 }
