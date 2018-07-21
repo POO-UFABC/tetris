f42727088abceb37b862d718dfba6788b2f84cfe
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-21 12:39:26 -0300

    Update Blocos.java

--- src/Blocos.java
+++ src/Blocos.java
@@ -1,37 +1,35 @@
-package entities;
-
 import java.awt.Color;
 import java.awt.Graphics;
+import javax.swing.JPanel;
 
 
-public class Blocos extends BaseShape {
-    private int x = 0;
+public class Blocos extends JPanel{
+        private int x = 0;
         private int y = 0;
+        private Color c;
         
-        public Blocos(int x, int y){
+        public Blocos(int x, int y, Color c){
             this.x = x;
-            this.y = y;            
+            this.y = y;   
+            this.c = c;
         }
         
          @Override
          public void paintComponent( Graphics g ) {
              
           super.paintComponent( g );
-          g.setColor(Color.DARK_GRAY);
+          g.setColor(this.c);
           g.fillRect(getX(),getY(), 15,15);
           g.drawRect(getX(),getY(),15,15 );
-         
-         
-          
-       }
+          }
     
    
-   public int getX(){
-       return this.x;
-   }
-   public int getY(){
-       return this.y;
-   }
+         public int getX(){
+             return this.x;
+         }
+        public int getY(){
+             return this.y;
+         }
    
     
 }
