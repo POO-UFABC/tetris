536bc5b754e79ca0468b532c92acf7003e7c9034
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 20:23:13 -0300

    Update PlayableStructureT.java

--- tetris/base/PlayableStructureT.java
+++ tetris/base/PlayableStructureT.java
@@ -3,14 +3,14 @@ package base;
 import java.awt.Color;
 
 public class PlayableStructureT extends PlayableStructure {	
-	private static final Color color = Color.orange;
+	private static final Color COLOR = Color.orange;
 	public PlayableStructureT(int x, int y){
 		super(x, y);
-		Block[][] Blocks = new Block[size][size];
-		Blocks[2][2] = new Block(color);
-		Blocks[2][3] = new Block(color);
-		Blocks[1][3] = new Block(color);
-		Blocks[3][3] = new Block(color);
-		this.setBlocks(Blocks);
+		Block[][] blocks = new Block[SIZE][SIZE];
+		Blocks[2][2] = new Block(COLOR);
+		Blocks[2][3] = new Block(COLOR);
+		Blocks[1][3] = new Block(COLOR);
+		Blocks[3][3] = new Block(COLOR);
+		this.setBlocks(blocks);
 	}
 }
