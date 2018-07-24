06cd8b25766b31416821bc2bc6c28903b924a796
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 20:24:12 -0300

    Update PlayableStructureZ.java

--- tetris/base/PlayableStructureZ.java
+++ tetris/base/PlayableStructureZ.java
@@ -3,14 +3,14 @@ package base;
 import java.awt.Color;
 
 public class PlayableStructureZ extends PlayableStructure {	
-	private static final Color color = Color.green;
+	private static final Color COLOR = Color.green;
 	public PlayableStructureZ(int x, int y){
 		super(x, y);
-		Block[][] Blocks = new Block[size][size];
-		Blocks[2][2] = new Block(color);
-		Blocks[3][2] = new Block(color);
-		Blocks[1][1] = new Block(color);
-		Blocks[2][1] = new Block(color);
-		this.setBlocks(Blocks);
+		Block[][] blocks = new Block[SIZE][SIZE];
+		Blocks[2][2] = new Block(COLOR);
+		Blocks[3][2] = new Block(COLOR);
+		Blocks[1][1] = new Block(COLOR);
+		Blocks[2][1] = new Block(COLOR);
+		this.setBlocks(blocks);
 	}
 }
