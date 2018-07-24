8afac0801b7caa9126d61ccf7cdc2c8abf69fd06
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 20:10:02 -0300

    Update PlayableStructureInvZ.java

--- tetris/base/PlayableStructureInvZ.java
+++ tetris/base/PlayableStructureInvZ.java
@@ -3,14 +3,14 @@ package base;
 import java.awt.Color;
 
 public class PlayableStructureInvZ extends PlayableStructure {	
-	private static final Color color = Color.yellow;
+	private static final Color COLOR = Color.yellow;
 	public PlayableStructureInvZ(int x, int y){
 		super(x, y);
 		Block[][] Blocks = new Block[size][size];
-		Blocks[2][2] = new Block(color);
-		Blocks[2][1] = new Block(color);
-		Blocks[3][1] = new Block(color);
-		Blocks[1][2] = new Block(color);
+		Blocks[2][2] = new Block(COLOR);
+		Blocks[2][1] = new Block(COLOR);
+		Blocks[3][1] = new Block(COLOR);
+		Blocks[1][2] = new Block(COLOR);
 		this.setBlocks(Blocks);
 	}
 }
