c821f1d4389930939c99988b20b68821b1df102e
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 20:17:35 -0300

    Update PlayableStructureL.java

--- tetris/base/PlayableStructureL.java
+++ tetris/base/PlayableStructureL.java
@@ -6,7 +6,7 @@ public class PlayableStructureL extends PlayableStructure {
 	private static final Color COLOR = Color.red;
 	public PlayableStructureL(int x, int y){
 		super(x, y);
-		Block[][] Blocks = new Block[size][size];
+		Block[][] Blocks = new Block[SIZE][SIZE];
 		Blocks[2][2] = new Block(COLOR);
 		Blocks[2][3] = new Block(COLOR);
 		Blocks[3][3] = new Block(COLOR);
