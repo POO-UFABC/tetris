a6760151cbfa8b57f25951ef5cfce0423dd3ce95
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 20:21:59 -0300

    Update PlayableStructureLine.java

--- tetris/base/PlayableStructureLine.java
+++ tetris/base/PlayableStructureLine.java
@@ -6,11 +6,11 @@ public class PlayableStructureLine extends PlayableStructure {
 	private static final Color COLOR = Color.pink;
 	public PlayableStructureLine(int x, int y){
 		super(x, y);
-		Block[][] Blocks = new Block[SIZE][SIZE];
+		Block[][] blocks = new Block[SIZE][SIZE];
 		Blocks[2][2] = new Block(COLOR);
 		Blocks[1][2] = new Block(COLOR);
 		Blocks[3][2] = new Block(COLOR);
 		Blocks[4][2] = new Block(COLOR);
-		this.setBlocks(Blocks);
+		this.setBlocks(blocks);
 	}
 }
