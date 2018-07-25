8924f222f62c0ac2fb3eba82f0c1b7662733af3a
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 21:22:40 -0300

    Update PlayableStructureInvZ.java

--- tetris/base/PlayableStructureInvZ.java
+++ tetris/base/PlayableStructureInvZ.java
@@ -7,10 +7,10 @@ public class PlayableStructureInvZ extends PlayableStructure {
 	public PlayableStructureInvZ(int x, int y){
 		super(x, y);
 		Block[][] blocks = new Block[SIZE][SIZE];
-		Blocks[2][2] = new Block(COLOR);
-		Blocks[2][1] = new Block(COLOR);
-		Blocks[3][1] = new Block(COLOR);
-		Blocks[1][2] = new Block(COLOR);
+		blocks[2][2] = new Block(COLOR);
+		blocks[2][1] = new Block(COLOR);
+		blocks[3][1] = new Block(COLOR);
+		blocks[1][2] = new Block(COLOR);
 		this.setBlocks(blocks);
 	}
 }
