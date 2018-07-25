da103d7b77ded039fc44f59f98c46c39c5f2b5e2
Costa-amanda <40688864+Costa-amanda@users.noreply.github.com>
2018-07-24 21:22:18 -0300

    Update PlayableStructureInvL.java

--- tetris/base/PlayableStructureInvL.java
+++ tetris/base/PlayableStructureInvL.java
@@ -7,10 +7,10 @@ public class PlayableStructureInvL extends PlayableStructure {
 	public PlayableStructureInvL(int x, int y){
 		super(x, y);
 		Block[][] blocks = new Block[SIZE][SIZE];
-		Blocks[2][2] = new Block(COLOR);
-		Blocks[2][3] = new Block(COLOR);
-		Blocks[1][3] = new Block(COLOR);
-		Blocks[2][1] = new Block(COLOR);
+		blocks[2][2] = new Block(COLOR);
+		blocks[2][3] = new Block(COLOR);
+		blocks[1][3] = new Block(COLOR);
+		blocks[2][1] = new Block(COLOR);
 		this.setBlocks(blocks);
 	}
 }
